b59b8462b7d993c2f704add3cad637733400e6ff
AndreJones216 <andrejones2162gmail.com>
2017-11-03 17:00:55 -0400

    Finised with initial mock. Added Moment to handle datetime

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 
+$(document).ready(function () {
+    console.log('App is running');
+});
+
+
+
+$("#getCommits").click(function(){
+    $.ajax({
+        type: "GET",
+        url: "https://api.github.com/repos/AndreJones216/Crossroads-Group/commits",
+        data: "data",
+        success: function (data) {
+            console.log(data[0]);
+            displayCommitInfo(data);
+        },
+        fail: function(e){
+            console.log('No luck getting any information from github' + e);    
+        }
+    });
+});
+
+function displayCommitInfo(gitCommits){
+    
+    var row = "";
+    var commitCount = gitCommits.length;
+
+    $.each(gitCommits, function (i, commit) { 
+        row +="<tr><td>" + commit.commit.author.name + "</td>";
+        row +="<td>" + commit.commit.message+ "</td>";
+        row +="<td>"+ moment(commit.commit.author.date).format("MMMM Do YYYY h:mma ") + "</td></tr>"
+    });
+
+    $("#commitsHTML").html(row);
+    $("#commitCount").html(commitCount);
+
+
+}
+
+
